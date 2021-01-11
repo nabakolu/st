d4f7e41ea2fc29fe998c1eaf8205b0574272c086
nabakolu <lnabakowski@mailo.com>
2021-01-11 14:51:35 +0100

    Changed font to Roboto Mono Nerd Font

--- config.h
+++ config.h
@@ -5,9 +5,8 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Liberation Mono:pixelsize=12:antialias=true:autohint=true";
+static char *font = "RobotoMono Nerd Font:pixelsize=16:antialias=true:autohint=true";
 static int borderpx = 0;
-
 /*
  * What program is execed by st depends of these precedence rules:
  * 1: program passed with -e
@@ -91,7 +90,7 @@ char *termname = "st-256color";
  *
  *	stty tabs
  */
-unsigned int tabspaces = 8;
+unsigned int tabspaces = 4;
 
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {

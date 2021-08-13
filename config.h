e8567f49b32ea5392261468ca3fd30df85a88119
nabakolu <lnabakowski@mailo.com>
2021-08-13 12:56:52 +0200

    Remove transparent background and let picom handle transparency

--- config.h
+++ config.h
@@ -133,7 +133,8 @@ static const char *colorname[] = {
  * foreground, background, cursor, reverse cursor
  */
 unsigned int defaultfg = 7;
-unsigned int defaultbg = 258;
+//unsigned int defaultbg = 258;
+unsigned int defaultbg = 0;
 static unsigned int defaultcs = 256;
 static unsigned int defaultrcs = 257;
 
